6a86f53a184fd48b9a0d4b2fa229c932543f9be7
mohammadazzuz <azzuzmohammad@outlook.de>
2025-06-21 14:12:26 +0200

    fix update9

--- safadprobot/routes/callback.py
+++ safadprobot/routes/callback.py
@@ -1,6 +1,6 @@
 # OAuth2 callback logic
 from flask import Blueprint, request, redirect, session
-from safadprobot.auth.discord_oauth import get_access_token, get_user_info
+from safadprobot.auth.discord_oauth import exchange_code, get_user_data
 
 callback_bp = Blueprint("callback", __name__)
 
@@ -10,8 +10,8 @@ def callback():
     if not code:
         return "Missing code from Discord", 400
 
-    token_data = get_access_token(code)
-    user_data = get_user_info(token_data["token_type"], token_data["access_token"])
+    token_data = exchange_code(code)
+    user_data = get_user_data(token_data["token_type"], token_data["access_token"])
 
     session["user"] = {
         "id": user_data["id"],
